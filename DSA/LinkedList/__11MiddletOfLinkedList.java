8ba9d335c2b789b911f3cbbabfd50aabae760ee0
yuvrajdas <yuvrajdas1531@gmail.com>
2024-01-07 21:46:25 +0530

    Middle of linkedlist

---
+++ DSA/LinkedList/__11MiddletOfLinkedList.java
@@ -0,0 +1,65 @@
+package DSA.LinkedList;
+
+public class __11MiddletOfLinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class Linkedlist {
+        int size;
+        Node head;
+        Node tail;
+
+        public void addLast(int val) {
+            if (size == 0) {
+                Node newNode = new Node();
+                newNode.data = val;
+                head = tail = newNode;
+                size++;
+            } else {
+                Node newNode = new Node();
+                newNode.data = val;
+                tail.next = newNode;
+                tail = newNode;
+                size++;
+            }
+        }
+
+        public void printList() {
+            Node temp = head;
+
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.print("null");
+            System.out.println();
+        }
+
+        public void getMiddleOfLinkedList() {
+            Node slow = head;
+            Node fast = head;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            System.out.println(slow.data);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Linkedlist ll = new Linkedlist();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.printList();
+        ll.getMiddleOfLinkedList();
+    }
+}
