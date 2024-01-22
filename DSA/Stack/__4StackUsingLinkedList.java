c5e30c056db56293a514fbb51f2adfea476bc7d5
yuvraj <yuvrajdas1531@gmail.com>
2024-01-22 17:51:43 +0530

    foreach map filter and reduce

---
+++ DSA/Stack/__4StackUsingLinkedList.java
@@ -0,0 +1,12 @@
+package DSA.Stack;
+
+import java.util.*;
+
+public class __4StackUsingLinkedList {
+    LinkedList<Integer> list;
+
+    public __4StackUsingLinkedList(){ }
+    public static void main(String[] args) {
+
+    }
+}
