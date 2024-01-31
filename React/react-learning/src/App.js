7275f416b174b0640708f9f2311150cb5a19ce0c
yuvraj <yuvrajdas1531@gmail.com>
2024-01-31 17:26:16 +0530

    class component and functional component view

--- React/react-learning/src/App.js
+++ React/react-learning/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
-
+import ClassComponentUser from './ClassComponentUser';
 function App() {
+  function Hello() {
+    return (
+      <>
+        <h1>I am Hello Component</h1>
+      </>
+    );
+  }
   return (
     <>
+      <ClassComponentUser />
+      <Hello />
       <h1>Hello</h1>
     </>
   );
---
+++ React/react-learning/src/ClassComponentUser.js
@@ -0,0 +1,11 @@
+
+import React, {Component} from 'react'
+
+class ClassComponentUser extends Component{
+    render(){
+        return <>
+            <h1>Hello i am class component</h1>
+        </>
+    }
+}
+export default ClassComponentUser;
