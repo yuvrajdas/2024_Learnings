206d82c52ef53abd8938be1dd32d78ad6d2e8e85
yuvrajdas <yuvrajdas1531@gmail.com>
2024-02-03 21:56:58 +0530

    location of expendature

--- React/sharpnear-react-assignments/src/HeaderExpence.jsx
+++ React/sharpnear-react-assignments/src/HeaderExpence.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './HeaderExpence.css';
 const HeaderExpence = () => {
+    let locationOfExpenditure = "Hyderabad";
     return (
         <>
             <h2>Expense Items</h2>
+            <p>Location of Expenditure <strong>{locationOfExpenditure}</strong></p>
             <ul>
                 <li>Food Rs 10</li>
                 <li>Petrol Rs 100</li>
