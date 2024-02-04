84bca1b8d46006b6979343f4862a920c681577de
yuvrajdas <yuvrajdas1531@gmail.com>
2024-02-04 11:21:32 +0530

    expense delete

--- React/sharpnear-react-assignments/src/App.js
+++ React/sharpnear-react-assignments/src/App.js
@@ -1,12 +1,21 @@
  
-import './App.css';
-import ExpenceDetails from './ExpenceDetails';
+import './App.css'; 
+import ExpenseDetails from './Expense/ExpenseDetails';
 import HeaderExpence from './HeaderExpence';
 
 function App() {
+
+  let expendeData = [
+    {date: '14-07-2020', title:"Toilet Paper", amount:94.6 },
+    {date: '15-07-2021', title:"New TV", amount:356 },
+    {date: '16-07-2022', title:"Car Insurance", amount:430 },
+    {date: '17-07-2023', title:"New Desk", amount:294.6 },
+    {date: '18-07-2023', title:"Bike", amount:450 }
+  ]
   return (
      <>
-      <HeaderExpence amount={1000} location="Raigarh" title="Expence Details" date={new Date().toDateString()}  />
+       <ExpenseDetails expenseData={ expendeData } />
+      {/* <HeaderExpence amount={1000} location="Raigarh" title="Expence Details" date={new Date().toDateString()}  /> */}
      </>
   );
 }
---
+++ React/sharpnear-react-assignments/src/Expense/ExpenseDetails.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './expense-details.css';
+
+const ExpenseDetails = (props) => {
+  const deleteExpense = (ele) => {
+    const updatedExpenses = props.expenseData.filter((expense) => expense !== ele);
+    props.updateExpenses(updatedExpenses);
+  };
+
+  return (
+    <>
+      <div className='expense-wrapper'>
+        <ul>
+          {props.expenseData.map((ele, index) => (
+            <li key={index}>
+              <span>{ele.date}</span>
+              <span>{ele.title}</span>
+              <span>{ele.amount}</span>
+              <button onClick={() => deleteExpense(ele)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseDetails;
