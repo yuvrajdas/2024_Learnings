77c5422bd6ec9be596d01db642808ac3a9bcb8f6
yuvrajdas <yuvrajdas1531@gmail.com>
2024-02-04 12:48:45 +0530

    state with class component

--- React/react-code-step-by-step/src/App.js
+++ React/react-code-step-by-step/src/App.js
@@ -1,9 +1,11 @@
 import State from "./Concepts/State";
+import StateWithClassComponent from "./Concepts/StateWithClassComponent";
 
 function App() {
   return (
    <>
     <State />
+    <StateWithClassComponent />
    </>
   );
 }
---
+++ React/react-code-step-by-step/src/Concepts/StateWithClassComponent.jsx
@@ -0,0 +1,26 @@
+import {Component} from "react";
+
+export default class StateWithClassComponent extends Component{
+    // eslint-disable-next-line no-useless-constructor
+    constructor(){
+        super();
+ 
+        // this.state = {
+        //     count : 0
+        // }
+    }
+    state = {
+        count : 0
+    }
+    changeValue(){
+        this.setState({count:this.state.count+1});
+    }
+    render(){
+        return(
+            <>  
+                <strong>{this.state.count}</strong>
+                <button onClick={()=>this.changeValue()}>Click Me</button>
+            </>
+        )
+    }
+}
