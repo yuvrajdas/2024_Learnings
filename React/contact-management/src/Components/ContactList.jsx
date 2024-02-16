fcbbe36b0d1e8fbe46e31207cc085e8d86d1a459
yuvraj <yuvrajdas1531@gmail.com>
2024-02-16 10:23:05 +0530

    Contact list card design

--- React/contact-management/src/Components/ContactList.jsx
+++ React/contact-management/src/Components/ContactList.jsx
@@ -21,22 +21,19 @@ const ContactList = () => {
         <div className="row mt-3">
           <div className="col-md-4 col-sm-6 mb-3">
             <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad cum fuga quia. Nisi cum non praesentium, quibusdam harum cupiditate soluta ab recusandae repellat repudiandae debitis, delectus ducimus adipisci consequuntur obcaecati.
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-6 mb-3">
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad cum fuga quia. Nisi cum non praesentium, quibusdam harum cupiditate soluta ab recusandae repellat repudiandae debitis, delectus ducimus adipisci consequuntur obcaecati.
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-6 mb-3">
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad cum fuga quia. Nisi cum non praesentium, quibusdam harum cupiditate soluta ab recusandae repellat repudiandae debitis, delectus ducimus adipisci consequuntur obcaecati.
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-6 mb-3">
-            <div className="card">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad cum fuga quia. Nisi cum non praesentium, quibusdam harum cupiditate soluta ab recusandae repellat repudiandae debitis, delectus ducimus adipisci consequuntur obcaecati.
+             <div className='d-flex justify-content-between p-3'>
+              <img style={{height:"80px"}} src="https://cdn.vectorstock.com/i/preview-1x/17/61/male-avatar-profile-picture-vector-10211761.jpg" alt="Profile_Img" />
+              <ul className='list-unstyled'>
+                <li>Name : <strong>Jhone Doe</strong></li>
+                <li>Mobile : <strong>1252458255</strong></li>
+                <li>Email : <strong>jhonedoe@gmail.com</strong></li>
+              </ul>
+             </div>
+             <div className='d-flex justify-content-end gap-3 pe-3 pb-3'>
+                <button className='btn btn-info '>Edit</button>
+                <button className='btn btn-warning'>View</button>
+                <button className='btn btn-danger'>Delete</button>
+              </div>
             </div>
           </div>
         </div>
