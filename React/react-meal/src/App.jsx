3bc47309a654d8893c3ae282ab0dd0722de08c63
yuvraj <yuvrajdas1531@gmail.com>
2024-02-26 17:18:06 +0530

    react meal app/add to cart

---
+++ React/react-meal/src/App.jsx
@@ -0,0 +1,11 @@
+import './css/App.css';
+import Header from './layout/Header'
+function App() {
+  return (
+   <>
+      <Header />
+   </>
+  );
+}
+
+export default App;
---
+++ React/react-meal/src/layout/Header.jsx
@@ -0,0 +1,8 @@
+import '../css/Header.css';
+const Header = ()=>{
+    return(<>
+        <h1>Header</h1>
+    </>)
+}
+
+export default Header;
